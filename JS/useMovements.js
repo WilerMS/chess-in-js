2622237410cb6038f91599beb94f7df9a215e6bb
Wiler Marinez <wilersoriano@gmail.com>
2021-09-26 09:34:51 +0200

    Refactoring pawn movements

--- JS/useMovements.js
+++ JS/useMovements.js
@@ -1,38 +1,26 @@
 const calculatePawnMovements    =   (posX, posY, matrix) => {
-    let movements = [];
+    let movements = [], diagMoves = [1, -1];
     let steps = (matrix[posX][posY].color === "white") ? [1,2] : [-1,-2];
 
     if (posX+steps[0]>=0 && posX+steps[0]<8) {
         if (!matrix[posX+steps[0]][posY]) {
             movements.push((posX+steps[0]).toString() + posY);
     
-            if (posX === 1 || posX === 6) {
-                if (posX+steps[1]>=0 && posX+steps[1]<8) {
-                    if (!matrix[posX+steps[1]][posY]) {
-                        movements.push((posX+steps[1]).toString() + posY);
-                    } 
-                }  
+            if ((posX === 1 || posX === 6) && posX+steps[1]>=0 && posX+steps[1]<8) {
+                (!matrix[posX+steps[1]][posY]) && (movements.push((posX+steps[1])+""+posY));
             }
         }
     }
     
-
-    if (posY-1 >= 0 && posY-1 <= 7) {
-        if (matrix[posX+steps[0]][posY-1]) {
-            if (matrix[posX+steps[0]][posY-1].user != matrix[posX][posY].user) {
-                movements.push((posX+steps[0]).toString() + (posY-1).toString());
+    for (let i = 0; i < 2; i++) {
+        if (posY+diagMoves[i] >= 0 && posY+diagMoves[i] <= 7) {
+            if (matrix[posX+steps[0]][posY+diagMoves[i]]) {
+                if (matrix[posX+steps[0]][posY+diagMoves[i]].user != matrix[posX][posY].user) {
+                    movements.push((posX+steps[0]).toString() + (posY+diagMoves[i]).toString());
+                }
             }
-        }
+        }   
     }
-
-    if (posY+1 >= 0 && posY+1 <= 7) {
-        if (matrix[posX+steps[0]][posY+1]) {
-            if (matrix[posX+steps[0]][posY+1].user != matrix[posX][posY].user) {
-                movements.push((posX+steps[0]).toString() + (posY+1).toString());
-            }
-        }
-    }
-
     return movements;
 }
 
